9c9e777119f70eb5626f1d237d3682485377b58e
Darshan Jagtap <153431135+darsh555787@users.noreply.github.com>
2025-02-06 13:47:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example of toggling content visibility
+document.querySelectorAll('.side-news li').forEach((item) => {
+    item.addEventListener('click', () => {
+      alert('More details coming soon!');
+    });
+  });
+  
